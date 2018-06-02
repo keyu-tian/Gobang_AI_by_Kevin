a492e639bf8a35a7aff42cb2b3c137c906879742
Kevin <39692511+Ke-Coding@users.noreply.github.com>
2018-06-03 00:13:28 +0800

    game.h
    游戏主要框架

---
+++ game.h
@@ -0,0 +1,10 @@
+#ifndef _GAME_H
+#define _GAME_H
+
+#include "base.h"
+
+void AIGame();               // 小猪佩棋对弈模式 
+void FriendsGame();          // 双人对弈模式 
+Point AITurn();              // AI回合
+
+#endif
