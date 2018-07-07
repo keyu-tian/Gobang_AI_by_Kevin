e6d1fd93db7f4eb58f025a46f977f435e70e3935
Kevin <39692511+Ke-Coding@users.noreply.github.com>
2018-07-08 01:37:45 +0800

    ai_evaluate.h
    分数评估相关函数声明

--- ai_evaluate.h
+++ ai_evaluate.h
@@ -1,7 +1,8 @@
 /**
+ * Name: ai_evaluate.h
  * Author: Kevin
  * Date: 2018-06-03
- * Description: 分数评估 
+ * Description: 分数评估
  */
 
 #ifndef _AI_EVALUATE_H
@@ -9,14 +10,14 @@
 
 #include "base.h"
 
-int  EvalBoard(int x, int y, int NowPlayer);  // 对当前棋局分数进行评估 
-int  EvalPrior(int x, int y, int NowPlayer);  // 对某一落子点进行优先级值评估 
+int  EvalBoard(int x, int y, int NowPlayer);	// 对当前棋局分数进行评估 
+int  EvalPrior(int x, int y, int NowPlayer);	// 对某一落子点进行优先级值评估 
 
-void AnalysisVertical  ();             // 分析棋盘纵向15路棋型 
-void AnalysisHorizontal();             // 分析棋盘横向15路棋型 
-void AnalysisInclined  ();             // 分析棋盘斜向42路棋型 
+void AnalysisVertically  ();					// 分析棋盘纵向15路棋型 
+void AnalysisHorizontally();					// 分析棋盘横向15路棋型 
+void AnalysisDiagonally  ();					// 分析棋盘斜向42路棋型 
 
-void ScanLine(const int* const Line, int n);  // 分析线型棋型（核心函数） 
+void ScanLine(const int* const Line, int n);	// 分析一路 线型棋型（核心函数） 
 
 
 #endif
