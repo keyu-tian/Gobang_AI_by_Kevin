f2feb4c25d44ebb2ad91b56fabdafdb33405043e
Kevin <39692511+Ke-Coding@users.noreply.github.com>
2018-09-24 00:11:36 +0800

    Update ai_algorithm.h
    AI关键算法

--- ai_algorithm.h
+++ ai_algorithm.h
@@ -1,22 +1,29 @@
 /**
+ * Name: ai_algorithm.h
  * Author: Kevin
  * Date: 2018-06-03
  * Description: AI关键算法
  */
 
-#ifndef _AI_ALGORITHM_H
-#define _AI_ALGORITHM_H
+#ifndef _AI_ALGO_H
+#define _AI_ALGO_H
 
 #include "base.h"
 
-int MinMaxSearch(int x, int y, int NowPlayer, int depth, int alpha, int beta); // 极大极小值搜索，使用了Alpha-Beta剪枝进行优化
+int minMaxSearch(Chessid NowPlayer, int depth, int alpha, int beta); // 极大极小值搜索，使用了Alpha-Beta剪枝进行优化 
 
-int HeuriSearchChoices(Choice* AvaChoices);     // 启发式搜索函数 生成待遍历可落子点序列
+int killSearch(Chessid NowPlayer, int depth, int alpha, int beta); // 极大极小值搜索，使用了Alpha-Beta剪枝进行优化 
 
-int ChoiceCMP(const void *a, const void *b);    // 用于快排的比较函数
+int heuriSearchChoices(Choice *AvaChoices);			// 启发式搜索函数 生成待遍历可落子点序列 
+
+int killSearchChoices(Choice *AvaChoices);			// 启发式搜索函数 生成待遍历可落子点序列 
+
+void quickSort(Choice *a, int n);
+void merge(Choice *a, int n_left, int n_right);
+void mergeSort(Choice *a, int n);
  
-Chess RandFirst();                              // 随机生成第一回合的AI落子点
+Point RandFirst();									// 随机生成第一回合的AI落子点
 
-int Win(int x, int y, int NowPlayer);           // 胜负判断
+int Win(int x, int y, Chessid NowPlayer);				// 胜负判断 
 
 #endif
