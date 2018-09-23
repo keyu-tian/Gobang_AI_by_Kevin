42e893090aad759ef82b968eca58578f7f0cfe9e
Kevin <39692511+Ke-Coding@users.noreply.github.com>
2018-09-24 00:06:01 +0800

    Update main.c
    main函数

--- main.c
+++ main.c
@@ -1,21 +1,25 @@
 #include <stdlib.h>
-#include "game.h"
+#include "main_game.h"
+#include "controller.h"
+#include "eval_board.h"
 
 int main()
 {
 	system("color F0");
-	system("mode con cols=100 lines=36");
+	system("mode con cols=100 lines=42");
 	int mode;
-
+	initHASH();
+	
 	while (1)
 	{
+
 		MenuPrint();
 
 		mode = ModeSelect();
 
-		if(mode==2)
+		if ( mode==2 )
 			FriendsGame(mode);
-		else
+		else // if ( mode==0 || mode==1 )
 			AIGame(mode);
 
 		system("cls");
