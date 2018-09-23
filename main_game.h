1ae95b93a4896864f9c5999b526f08f24832d830
Kevin <39692511+Ke-Coding@users.noreply.github.com>
2018-09-24 00:06:59 +0800

    Create main_game.h
    游戏主要框架

---
+++ main_game.h
@@ -0,0 +1,17 @@
+/**
+ * Name: game.h
+ * Author: Kevin
+ * Date: 2018-06-03
+ * Description: 游戏主要框架 
+ */
+
+#ifndef _MAIN_GAME_H
+#define _MAIN_GAME_H
+
+#include "base.h"
+
+void AIGame(int mode);							// 小猪佩棋对弈模式 
+void FriendsGame(int mode);						// 双人对弈模式 
+Point AITurn();									// AI回合
+
+#endif
